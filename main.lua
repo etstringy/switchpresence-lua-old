3a9caefd651d303ef17db6d26ba32bcf648c8882
etstringy <34780309+etstringy@users.noreply.github.com>
2019-01-21 18:01:20 +0000

    Add SP 2 source code

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local discordRPC = require("discordRPC")
 local appId = require("applicationId")
+local utf8 = require("utf8")
 
 function discordRPC.ready(userId, username, discriminator, avatar)
     print(string.format("Discord: ready (%s, %s, %s, %s)", userId, username, discriminator, avatar))
@@ -15,120 +16,98 @@ end
 
 
 function love.load()
-	game = "None"
+	bold = love.graphics.setNewFont("SanFranciscoDisplay-Bold.ttf", 18)
 
-	smash = {
+	love.graphics.setBackgroundColor("255", "0", "0")
+	image = love.graphics.newImage( "logowhite.png" )
+	text = ""
+	presence = {
 	    largeImageKey = "ns",
-	    smallImageKey = "smash",
-	    smallImageText = "Super Smash Bros Ultimate",
-        details = "Super Smash Bros Ultimate"
+        details = "Idle",
+        smallImageKey = "nil"
     }
-   	splatoon = {
-	    largeImageKey = "ns",
-	    smallImageKey = "splatoon_2",
-	    smallImageText = "Splatoon 2",
-        details = "Splatoon 2"
-    }
-	odyssey = {
-	    largeImageKey = "ns",
-	    smallImageKey = "super_mario_odyssey",
-	    smallImageText = "Super Mario Odyssey",
-        details = "Super Mario Odyssey"
-    }
-	fortnite = {
-	    largeImageKey = "ns",
-	    smallImageKey = "fn",
-	    smallImageText = "Fortnite",
-        details = "Fortnite"
-    }
-	mk8d = {
-	    largeImageKey = "ns",
-	    smallImageKey = "mk8d",
-	    smallImageText = "Mario Kart 8 Deluxe",
-        details = "Mario Kart 8 Deluxe"
-    }
-	mc = {
-	    largeImageKey = "ns",
-	    smallImageKey = "mc",
-	    smallImageText = "Minecraft",
-        details = "Minecraft"
-    }
-	botw = {
-	    largeImageKey = "ns",
-	    smallImageKey = "botw",
-	    smallImageText = "The Legend of Zelda: BoTW",
-        details = "The Legend of Zelda: BoTW"
-    }
-	yt = {
-	    largeImageKey = "ns",
-	    smallImageKey = "yt",
-	    smallImageText = "YouTube",
-        details = "YouTube"
-    }
-
 	discordRPC.initialize(appId, true)
+	discordRPC.updatePresence(presence)
 end
 
+function love.textinput(t)
+	text = text .. t
+	presence.details = text
+	if presence.details == "Super Smash Bros Ultimate" then
+		presence.smallImageKey = "smash"
+	elseif presence.details == "Fortnite" then
+	    presence.smallImageKey = "fn"
+	elseif presence.details == "Minecraft" then
+	    presence.smallImageKey = "mc"
+	elseif presence.details == "Splatoon 2" then
+		presence.smallImageKey = "splatoon_2"
+	elseif presence.details == "YouTube" then
+		presence.smallImageKey = "yt"
+	elseif presence.details == "Undertale" then
+	    presence.smallImageKey = "undertale"
+    elseif presence.details == "Super Mario Odyssey" then
+    	presence.smallImageKey = "super_mario_odyssey"
+	else
+	    presence.smallImageKey = "nil"
+	end
 
+	discordRPC.updatePresence(presence)
+end
+
+function love.keypressed(key)
+    if key == "backspace" then
+        -- get the byte offset to the last UTF-8 character in the string.
+        local byteoffset = utf8.offset(text, -1)
+ 
+        if byteoffset then
+            -- remove the last UTF-8 character.
+            -- string.sub operates on bytes rather than UTF-8 characters, so we couldn't do string.sub(text, 1, -2).
+            text = string.sub(text, 1, byteoffset - 1)
+        end
+
+    	if presence.details == "Super Smash Bros Ultimate" then
+			presence.smallImageKey = "smash"
+		elseif presence.details == "Fortnite" then
+		    presence.smallImageKey = "fn"
+		elseif presence.details == "Minecraft" then
+		    presence.smallImageKey = "mc"
+		elseif presence.details == "Splatoon 2" then
+			presence.smallImageKey = "splatoon_2"
+		elseif presence.details == "YouTube" then
+			presence.smallImageKey = "yt"
+		elseif presence.details == "Undertale" then
+		    presence.smallImageKey = "undertale"
+	    elseif presence.details == "Super Mario Odyssey" then
+	    	presence.smallImageKey = "super_mario_odyssey"
+		else
+		    presence.smallImageKey = "nil"
+		end
+    	presence.details = text
+		discordRPC.updatePresence(presence)
+    end
+
+    if key == "escape" then
+    	love.event.quit()
+    end
+end
 
 function love.draw()
-	gamestr = [[ Playing: ]] .. game 
-	love.graphics.setNewFont("PRESSSTART2P.TTF")
 	love.graphics.setColor( 255, 255, 255 )
-	love.graphics.print("SwitchPresence", "50", "50")
-	love.graphics.setColor( 255, 0, 0 )
-	love.graphics.print("Press ESC at any time to quit", "50", "100")
+	love.graphics.print("SwitchPresence", "90", "20")
+	love.graphics.draw(image, "55", "50", "0", "0.5", "0.5")
 	love.graphics.setColor( 0, 255, 0 )
-	love.graphics.print("Press 1 for Splatoon 2", "50", "125")
-	love.graphics.print("Press 2 for Smash Ultimate", "50", "150")
-	love.graphics.print("Press 3 for Super Mario Odyssey", "50", "175")
-	love.graphics.print("Press 4 for Fortnite", "50", "200")
-	love.graphics.print("Press 5 for Mario Kart 8 Deluxe", "50", "225")
-	love.graphics.print("Press 6 for Minecraft", "50", "250")
-	love.graphics.print("Press 7 for The Legend of Zelda: Breath of the Wild", "50", "275")
-	love.graphics.print("Press 8 for YouTube", "50", "300")
-	love.graphics.setColor( 0, 255, 0 )
-	love.graphics.print(gamestr, "550", "50")
+	love.graphics.print("Type away!", "50", "280")
+	love.graphics.print(">", "30", "300")
+	love.graphics.setColor( 255, 255, 255 )
+	love.graphics.printf(displaytext, 50, 300, love.graphics.getWidth())
+	-- love.graphics.rectangle("fill", 280, 50, 200, 250)
 end
 
 function love.update(dt)
-	if love.keyboard.isDown("escape") then
-		love.event.quit()
-	end
-	if love.keyboard.isDown("2") then
-		discordRPC.updatePresence(smash)
-		game = "Smash"
-	end
-	if love.keyboard.isDown("1") then
-		discordRPC.updatePresence(splatoon)
-		game = "Splatoon2"
-	end
-	if love.keyboard.isDown("3") then
-		discordRPC.updatePresence(odyssey)
-		game = "SMOdyssey"
-	end
-	if love.keyboard.isDown("4") then
-		discordRPC.updatePresence(fortnite)
-		game = "Fortnite"
-	end
-	if love.keyboard.isDown("5") then
-		discordRPC.updatePresence(mk8d)
-		game = "MarioKart"
-	end
-	if love.keyboard.isDown("6") then
-		discordRPC.updatePresence(mc)
-		game = "Minecraft"
-	end
-	if love.keyboard.isDown("7") then
-		discordRPC.updatePresence(botw)
-		game = "Zelda BoTW"
-	end
-	if love.keyboard.isDown("8") then
-		discordRPC.updatePresence(yt)
-		game = "Youtube"
-	end
+	displaytext = text .. "|"
 end
 
 function love.quit()
+	print("Quit called")
 	discordRPC.shutdown()
 end
